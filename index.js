481446be6a4405ef91d6e352ec8890fdcd4ebdf0
Evin Owen <github@evinowen.net>
2020-05-01 15:07:55 -0700

    Add send_request function for making request to relay destination

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-var keypress = require('keypress');
+const http = require('http')
+const keypress = require('keypress');
 
 keypress(process.stdin);
+
+function send_request(hostname, port, player, key) {
+  console.log('send_request', hostname, port, player, key)
+
+  return new Promise((resolve, reject) => {
+    const options = {
+      hostname: hostname,
+      port: port,
+      path: `/player${player}/${key}`,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': 0
+      }
+    }
+
+    const req = http.request(options, res => {
+      console.log(`statusCode: ${res.statusCode}`)
+      resolve(res.statusCode)
+    })
+
+    req.on('error', reject)
+
+    req.end()
+  })
+}
 
 process.stdin.on('keypress', function (ch, key) {
   if (key && key.ctrl && key.name == 'c') {
